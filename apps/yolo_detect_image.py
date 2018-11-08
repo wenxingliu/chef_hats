b82259962a7acd1aa553164871ff6f7d80e1af53
deep_learning <34463783+Coder-WangZe@users.noreply.github.com>
2018-11-08 13:49:21 +0800

    Add files via upload

--- apps/yolo_detect_image.py
+++ apps/yolo_detect_image.py
@@ -14,7 +14,7 @@ def detect_image(yolo, input_image, output_image_dir, show_img=False):
     image = Image.open(input_image)
 
     image_name = os.path.split(input_image)[1]
-    # png image can't be detected directly
+    # convert jpg to png image
     if image_name.endswith(".png"):
         jpg_image = Image.new("RGB", image.size, (255, 255, 255))
         jpg_image.paste(image)
@@ -23,15 +23,21 @@ def detect_image(yolo, input_image, output_image_dir, show_img=False):
         new_img = yolo.detect_image(image)
     if show_img:
         new_img.show()
-    name = output_image_dir + image_name[0: -4]
-    new_img.save(name + '.jpg')
+    name = output_image_dir + image_name
+    new_img.save(name)
 
 
+# os.chdir('D:\\hat_mask_detection\\')
+# yolo = YOLO()
+# image_dir = "D:\\hat_mask_detection\\kitchen_data\\image2\\153.png"
+# output_image_dir = "D:\\hat_mask_detection\\output_image\\"
+# detect_image(yolo, image_dir, output_image_dir)
+# yolo.close_session()
 
 start = timer()
 yolo = YOLO()
-all_image_dir = 'D:\\hat_mask_detection\\kitchen_data\\train_image\\'
-output_image_dir = "D:\\hat_mask_detection\\output_image\\"
+all_image_dir = 'D:\\project3\\chef_hats\\raw_image_data\\all_image\\'
+output_image_dir = "D:\\project3\\chef_hats\\output_image\\output\\"
 image_list = os.listdir(all_image_dir)
 image_dirs = [all_image_dir + image for image in image_list]
 for image_dir in image_dirs:
